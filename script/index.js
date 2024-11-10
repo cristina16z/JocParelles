16a82072285b8a59024f80b6e89daef43e7fb5f3
cristina16z <cristina16vm@gmail.com>
2024-11-10 20:39:18 +0100

    Alert de No empezar otra partida, si hay una en curso y no ha finalizado

--- script/index.js
+++ script/index.js
@@ -21,13 +21,20 @@ let estadoGame = "No hi ha cap partida en joc";
 const userAgent = navigator.userAgent;
 
 
-
 /****************************************************************** FUNCIONALITATS ********************************************************/
 
 function empezarPartida(){
+
+    if (localStorage.getItem("partidaActiva") === "true"){          //Verificamos si ya hay una ventana de juego activa previamente
+        pagina = window.open('joc.html');                           //si la hay, abrimos y cerramos el nuevo juego, y mostramos alerta
+        alert("Hi ha una partida començada"); 
+        pagina.close();                                             
+    }
+
     if(obj_nom.value){
         pagina = window.open('joc.html');
         document.cookie ="username=" + obj_nom.value;               //Guardamos el nombre en una cookie
+        localStorage.setItem("partidaActiva", "true");              //Decimos que ya hay una partidaActiva
     }else{
         alert(`Has d'introduir el nom del jugador`);
     }
@@ -39,19 +46,23 @@ function borrarPartida(){
     localStorage.removeItem("localpunts");
     localStorage.removeItem("localjugador");
     obj_nom.value="";
-    pagina.close('joc.html');                                        //cerramos la ventana de juego
-    obj_info_puntuacio.textContent =  "No hi ha cap partida en joc"; //actualizamos el estado
+    localStorage.setItem("partidaActiva", "false");                     //decimos que ya no hay partida activa
 
+    if (pagina && !pagina.closed) {
+    pagina.close('joc.html');                                           //cerramos la ventana de juego activa si existe y no está cerrada
+    }      
+                          
+    obj_info_puntuacio.textContent =  "No hi ha cap partida en joc";    //actualizamos el estado
 }
 
 
-function infoNavegador(){                                           //Extracció de la info del Navegador
+function infoNavegador(){                                               //Extracció de la info del Navegador
     obj_info_navegador.textContent = userAgent;
 }
 
 
 
-function infoURL(){                                                 //Extracció de la info de la url
+function infoURL(){                                                     //Extracció de la info de la url
     const url = location.origin;
     obj_info_url.textContent = url;
 }
@@ -66,6 +77,10 @@ function canalInfo() {
             const jugador = event.data.jugador;
             const puntos = event.data.puntos;
             const estado = event.data.estado;
+
+            if (estado === "Partida Finalitzada") {                 //Si finalizamos una partida, partidaActiva a false para que se pueda empezar otra partida 
+                localStorage.setItem("partidaActiva", "false");     // y no salte el alert de que ya hay una partida empezada
+            }
         
             if (estado === "No hi ha cap partida en joc") {
                 obj_info_puntuacio.textContent = estado;
